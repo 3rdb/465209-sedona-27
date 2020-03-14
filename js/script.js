17f862aa4ed35f4fe55f31296c113759e8386211
3rdb <chumorf@gmail.com>
2020-03-14 19:50:55 +0300

    все поправил + js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var link = document.querySelector(".reserve-button");
+var popup = document.querySelector(".popup-form");
+
+var form = popup.querySelector("form");
+var datein = popup.querySelector("[name=date-check-in]");
+var dateoff = popup.querySelector("[name=date-check-off]");
+
+
+
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("popup-form-close");
+    popup.classList.remove("popup-form-shake");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!datein.value || !dateoff.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести даты въезда и выезда");
+      popup.classList.remove("popup-form-shake");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("popup-form-shake");
+    }
+  });
